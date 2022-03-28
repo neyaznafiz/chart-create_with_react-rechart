8ac52b71ba27d132cdd9de3f5780f297abe6ea1b
niiyazobug <niiyaz5555@gmail.com>
2022-03-28 21:08:07 +0600

    XAxis YAxis added

--- src/components/MyLineChart/MyLineChart.js
+++ src/components/MyLineChart/MyLineChart.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Line, LineChart } from 'recharts';
+import { Line, LineChart, XAxis, YAxis } from 'recharts';
 
 const MyLineChart = () => {
 
@@ -7,51 +7,54 @@ const MyLineChart = () => {
         {
             name: 'Supplier A',
             price: 4000,
-            pv: 2400,
+            sales: 2400,
             amt: 2400,
         },
         {
             name: 'Supplier B',
             price: 3000,
-            pv: 1398,
+            sales: 1398,
             amt: 2210,
         },
         {
             name: 'Supplier C',
             price: 2000,
-            pv: 9800,
+            sales: 9800,
             amt: 2290,
         },
         {
             name: 'Supplier D',
             price: 2780,
-            pv: 3908,
+            sales: 3908,
             amt: 2000,
         },
         {
             name: 'Supplier E',
             price: 1890,
-            pv: 4800,
+            sales: 4800,
             amt: 2181,
         },
         {
             name: 'Supplier F',
             price: 2390,
-            pv: 3800,
+            sales: 3800,
             amt: 2500,
         },
         {
             name: 'Supplier G',
             price: 3490,
-            pv: 4300,
+            sales: 4300,
             amt: 2100,
         },
     ];
 
     return (
-        <LineChart width={400} height={500} data={data}>
+        <LineChart width={1000} height={500} data={data}>
 
             <Line dataKey={'price'}></Line>
+            <Line dataKey={'sales'}></Line>
+            <XAxis dataKey='name'></XAxis>
+            <YAxis></YAxis>
 
         </LineChart>
     );
